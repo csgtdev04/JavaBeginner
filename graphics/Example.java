d24c3a7ad3b9a38e4b2a1282c99b05e9e9b1625d
csgtdev04 <62195113+csgtdev04@users.noreply.github.com>
2021-11-13 12:06:18 -0600

    graphics fix

--- graphics/Example.java
+++ graphics/Example.java
@@ -8,7 +8,7 @@ public class Example {
         DrawingPanel drawingPanel = new DrawingPanel(1000, 1000); // paper
         Graphics drawingPen = drawingPanel.getGraphics(); // pen
 
-        drawABunchOfRectanglesAndOvals(drawingPen);
+//        drawABunchOfRectanglesAndOvals(drawingPen);
 
         int numTimes = 3;
         drawSquaresSidebySide(drawingPen, numTimes);
@@ -17,6 +17,14 @@ public class Example {
 
     public static void drawSquaresSidebySide(Graphics drawingPen, int numTimes) {
         // do the implementation for this method
+        int xOffset = 50;
+        int xStart = 50;
+        int yStart = 100;
+        int size = 70;
+
+        for (int square = 0; square < numTimes; square++) {
+            drawingPen.drawRect(xStart + (size * square), yStart, size, size);
+        }
     }
 
     public static void drawABunchOfRectanglesAndOvals(Graphics drawingPen) {
