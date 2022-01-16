bf8e07d097fc55bb610e15f6adb24cfbfe4c7d6a
csgtdev04 <62195113+csgtdev04@users.noreply.github.com>
2022-01-16 11:02:49 -0600

    FileTesting

--- FileTesting.java
+++ FileTesting.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class FileTesting {
     public static void main(String[] args) throws FileNotFoundException {
         // 1. read in the file
-        // - create the file
-        File file = new File("data.txt"); // represent our data.txt (tells our java file that
-        // another file with the name data.txt exists)
+        // - create the file object (accessing it/locating it)
+        File file = new File("datafiles/data.txt");
 
-        // - read in the file using a scanner
+        // - read in the file using a scanner (transferring the file for reading purposes)
         Scanner fileScanner = new Scanner(file); // lets us look through our file
 
         // 2. analyze the file...
         // in order to print the data, we need to go through the data
 
         // - get the line
-        String firstLine = fileScanner.nextLine();
+        String firstLine = fileScanner.nextLine(); // whole line!
 
+        // Sai   18    UT
         // - make scanner for that line so you can read from that
         Scanner lineScanner = new Scanner(firstLine);
 
@@ -29,6 +29,7 @@ public class FileTesting {
         // - print in a readable way
         System.out.println(name + " is " + age + " and goes to " + school);
 
-
+        // WHILE LOOP [happens till condition = false]:
+        // we want to read lines from the file until we reach the end...
     }
 }
